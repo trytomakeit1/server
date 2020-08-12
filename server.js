61405cf4118112c4b10a680dc6e48b88b98a23e3
trytomakeit1 <trytomakeit1@gmail.com>
2020-08-12 10:56:35 +0430

    created https server

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ var express = require('express');
 var api = require('./api/api');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var http = require('http');
+//to add https
+var https = require('https');
+
+var fs = require('fs');
 
 var app = express();
+
 
 app.get("/", function(req, res){
     res.send("Hello world");
@@ -16,4 +22,15 @@ app.use(bodyParser.json());
 //* using cors module, can use same origin (localhost) requests.
 app.use("/api",cors(), api);
 
-app.listen(8080, function(){console.log("listening to port 8080");});
+var key = fs.readFileSync('./bin/2179541_localhost.key');
+var cert = fs.readFileSync('./bin/2179541_localhost.cert');
+
+var options = {
+    key: key,
+    cert: cert
+};
+
+var server = http.createServer(app);
+server.listen(8080, function(){console.log("listening to port 8080");});
+
+https.createServer(options, app).listen(4433, function(){console.log("listening to port 4433")});
